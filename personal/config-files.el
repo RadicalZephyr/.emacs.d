ed6b3b20b6fd92cbcee853034b728fa19f5bd4ba
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-26 06:08:40 -0700

    Configure anaconda-mode to install into the vendor folder and be hidden

---
+++ personal/config-files.el
@@ -0,0 +1,29 @@
+;;; config-files.el --- configure where various packages store data  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Zefira Shannon
+
+;; Author: Zefira Shannon <zefs@pop-os>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-after-load 'anaconda-mode
+  '(progn
+     (setq anaconda-mode-installation-directory
+           (expand-file-name "anaconda-mode" prelude-vendor-dir))))
+
+;;; config-files.el ends here
