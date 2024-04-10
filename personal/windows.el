f012a292c07d5e552d0bfbdf80fdad776df21a4b
Zefira Shannon <zefira@hey.com>
2024-04-10 04:50:30 -0700

    Set Windowns git binary path correctly

--- personal/windows.el
+++ personal/windows.el
@@ -1,3 +1,3 @@
 ;; Windows specific changes
 (when (eq system-type 'windows-nt)
-  (setq magit-git-executable "C:\\Program Files (x86)\\Git\\bin\\git.exe"))
+  (setq magit-git-executable "C:\\Program Files\\Git\\bin\\git.exe"))
