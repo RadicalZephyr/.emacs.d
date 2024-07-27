3de9073bf3e25d368bf4375505d553f651f1df72
Zefira Shannon <earthlingzephyr@gmail.com>
2024-07-27 06:40:17 -0700

    Make racket paredit key rebinding more sophisticated

--- personal/racket.el
+++ personal/racket.el
@@ -1,9 +1,14 @@
 (require 'use-package)
 (require 'paredit)
 
+;; I think this is unbinding 'paredit-newline because that triggers
+;; local reindentation which conflicts with racket-mode's electric
+;; reindentation.
+(defun radz-unbind-paredit-newline ()
+  (dolist (k '("RET" "C-m" "C-j"))
+    (define-key paredit-mode-map (kbd k) nil)))
+
 (use-package racket-mode
   :ensure t
   :mode "\\.rkt\\'"
-  :config
-  (dolist (k '("RET" "C-m" "C-j"))
-    (define-key paredit-mode-map (kbd k) nil)))
+  :hook (racket-mode . radz-unbind-paredit-newline))
