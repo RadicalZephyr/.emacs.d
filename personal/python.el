0c60efc75aae880be55636d177a96cb7ddfcd330
Zefira Shannon <earthlingzephyr@gmail.com>
2024-07-26 17:40:15 -0700

    Transition python setup to use-package

--- personal/python.el
+++ personal/python.el
@@ -1,19 +1,20 @@
-(prelude-require-packages '(
-                            pytest
-                            pipenv
-                            ))
+(require 'use-package)
 
-(require 'pytest)
+(use-package pipenv
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook #'pipenv-mode))
 
-(add-hook 'python-mode-hook #'pipenv-mode)
-
-(add-hook 'python-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-c C-a") 'pytest-all)
-            ;; (local-set-key "\C-cm" 'pytest-module)
-            ;; (local-set-key "\C-c." 'pytest-one)
-            ;; (local-set-key "\C-cd" 'pytest-directory)
-            ;; (local-set-key "\C-cpa" 'pytest-pdb-all)
-            ;; (local-set-key "\C-cpm" 'pytest-pdb-module)
-            ;; (local-set-key "\C-cp." 'pytest-pdb-one)
-            ))
+(use-package pytest
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (local-set-key (kbd "C-c C-a") 'pytest-all)
+              ;; (local-set-key "\C-cm" 'pytest-module)
+              ;; (local-set-key "\C-c." 'pytest-one)
+              ;; (local-set-key "\C-cd" 'pytest-directory)
+              ;; (local-set-key "\C-cpa" 'pytest-pdb-all)
+              ;; (local-set-key "\C-cpm" 'pytest-pdb-module)
+              ;; (local-set-key "\C-cp." 'pytest-pdb-one)
+              )))
