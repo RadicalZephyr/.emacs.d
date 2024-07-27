35a40b245cee80f683756a81b529dea2ecfbc8d4
Zefira Shannon <earthlingzephyr@gmail.com>
2024-07-26 17:05:26 -0700

    Switch from prelude js/ts modules to jtsx

--- personal/prelude-modules.el
+++ personal/prelude-modules.el
@@ -75,7 +75,7 @@
 ;; (require 'prelude-elixir)
 ;; (require 'prelude-go)
 ;; (require 'prelude-haskell)
-(require 'prelude-js)
+;; (require 'prelude-js)
 ;; (require 'prelude-latex)
 (require 'prelude-lisp) ;; Common setup for Lisp-like languages
 (require 'prelude-lsp) ;; Base setup for the Language Server Protocol
@@ -90,7 +90,7 @@
 ;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
-(require 'prelude-ts)
+;; (require 'prelude-ts)
 (require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
 (require 'prelude-yaml)
