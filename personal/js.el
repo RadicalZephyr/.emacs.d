7472fd6a5b275b9ec98f8fc94d06c3aa36c1cc77
Zefira Shannon <earthlingzephyr@gmail.com>
2024-08-01 16:20:09 -0700

    Change js/ts indent level back to 4

--- personal/js.el
+++ personal/js.el
@@ -21,8 +21,8 @@
          (jtsx-typescript-mode . hs-minor-mode))
   :custom
   ;; Optional customizations
-  (js-indent-level 2)
-  (typescript-ts-mode-indent-offset 2)
+  (js-indent-level 4)
+  (typescript-ts-mode-indent-offset 4)
   (jtsx-switch-indent-offset 0)
   (jtsx-indent-statement-block-regarding-standalone-parent nil)
   (jtsx-jsx-element-move-allow-step-out t)
