11800911039d604848968111fcea86d465aa73eb
Zefira Shannon <zefira@hey.com>
2025-09-09 16:33:56 -0700

    Add more tide keybinds and configure in jtsx-typescript-mode as well

--- personal/js.el
+++ personal/js.el
@@ -17,22 +17,22 @@
          (jtsx-typescript-mode . tide-hl-identifier-mode))
 
   :config
-  (defun tide-bind-keys-to-mode-map (mode-map)
+  (defun radz/tide-bind-keys-to-mode-map (mode-map)
     "Bind useful keys to MODE-MAP"
+    (define-key mode-map (kbd "C-c C-l e") 'tide-refactor)
     (define-key mode-map (kbd "C-c C-l d") 'tide-documentation-at-point)
     (define-key mode-map (kbd "C-c C-l D") 'tide-jsdoc-template)
+    (define-key mode-map (kbd "C-c C-l I") 'tide-add-eslint-disable-next-line)
     (define-key mode-map (kbd "C-c C-l r") 'tide-rename-symbol)
     (define-key mode-map (kbd "C-c C-l ?") 'tide-references)
     (define-key mode-map (kbd "C-c C-l x") 'tide-restart-server))
 
-  (defun jtsx-bind-keys-to-jtsx-jsx-mode-map ()
-    (tide-bind-keys-to-mode-map jtsx-jsx-mode-map))
-
-  (defun jtsx-bind-keys-to-jtsx-tsx-mode-map ()
-    (tide-bind-keys-to-mode-map jtsx-tsx-mode-map))
-
-  (add-hook 'jtsx-jsx-mode-hook 'tide-bind-keys-to-jtsx-jsx-mode-map)
-  (add-hook 'jtsx-tsx-mode-hook 'tide-bind-keys-to-jtsx-tsx-mode-map))
+  (add-hook 'jtsx-jsx-mode-hook
+            #'(lambda () (radz/tide-bind-keys-to-mode-map jtsx-jsx-mode-map)))
+  (add-hook 'jtsx-typescript-mode-hook
+            #'(lambda () (radz/tide-bind-keys-to-mode-map jtsx-typescript-mode-map)))
+  (add-hook 'jtsx-tsx-mode-hook
+            #'(lambda () (radz/tide-bind-keys-to-mode-map jtsx-tsx-mode-map))))
 
 (use-package jtsx
   :ensure t
