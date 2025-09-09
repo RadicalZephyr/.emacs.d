f09f1dd65915447abbee077238d38121eb446268
Zefira Shannon <zefira@hey.com>
2025-09-08 23:40:25 -0700

    Add nerd-icons and nerd-icons-dired

--- personal/fonts.el
+++ personal/fonts.el
@@ -1,3 +1,16 @@
+(require 'use-package)
+
+(use-package nerd-icons
+  :ensure t
+  :custom
+  (nerd-icons-font-family "Symbols Nerd Font Mono"))
+
+(use-package nerd-icons-dired
+  :ensure t
+  :after nerd-icons
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
 (when (window-system)
   (set-frame-font "Fira Mono"))
 
