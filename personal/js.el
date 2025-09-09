8c8cf2aa17c55922d1bc0dc17352fc77a3b8fb2b
Zefira Shannon <zefira@hey.com>
2025-09-09 16:30:18 -0700

    Switch back to tide from using lsp mode with ts-ls
    It was just too laggy, it made editing unreliable and kind of unbearable.

--- personal/js.el
+++ personal/js.el
@@ -8,39 +8,39 @@
          (jtsx-typescript-mode . prettier-js-mode)
          (jtsx-tsx-mode . prettier-js-mode)))
 
-;; (use-package tide
-;;   :ensure t
-;;   :after (company flycheck jtsx)
-;;   :hook ((jtsx-jsx-mode . tide-setup)
-;;          (jtsx-typescript-mode . tide-setup)
-;;          (jtsx-tsx-mode . tide-setup)
-;;          (jtsx-typescript-mode . tide-hl-identifier-mode))
+(use-package tide
+  :ensure t
+  :after (company jtsx)
+  :hook ((jtsx-jsx-mode . tide-setup)
+         (jtsx-typescript-mode . tide-setup)
+         (jtsx-tsx-mode . tide-setup)
+         (jtsx-typescript-mode . tide-hl-identifier-mode))
 
-;;   :config
-;;   (flycheck-add-mode 'javascript-eslint 'jtsx-jsx-mode)
-;;   (flycheck-add-mode 'javascript-eslint 'jtsx-typescript-mode)
-;;   (flycheck-add-mode 'javascript-eslint 'jtsx-tsx-mode)
+  :config
+  (flycheck-add-mode 'javascript-eslint 'jtsx-jsx-mode)
+  (flycheck-add-mode 'javascript-eslint 'jtsx-typescript-mode)
+  (flycheck-add-mode 'javascript-eslint 'jtsx-tsx-mode)
 
-;;   (defun tide-bind-keys-to-mode-map (mode-map)
-;;     "Bind useful keys to MODE-MAP"
-;;     (define-key mode-map (kbd "C-c C-l d") 'tide-documentation-at-point)
-;;     (define-key mode-map (kbd "C-c C-l D") 'tide-jsdoc-template)
-;;     (define-key mode-map (kbd "C-c C-l r") 'tide-rename-symbol)
-;;     (define-key mode-map (kbd "C-c C-l ?") 'tide-references)
-;;     (define-key mode-map (kbd "C-c C-l x") 'tide-restart-server))
+  (defun tide-bind-keys-to-mode-map (mode-map)
+    "Bind useful keys to MODE-MAP"
+    (define-key mode-map (kbd "C-c C-l d") 'tide-documentation-at-point)
+    (define-key mode-map (kbd "C-c C-l D") 'tide-jsdoc-template)
+    (define-key mode-map (kbd "C-c C-l r") 'tide-rename-symbol)
+    (define-key mode-map (kbd "C-c C-l ?") 'tide-references)
+    (define-key mode-map (kbd "C-c C-l x") 'tide-restart-server))
 
-;;   (defun jtsx-bind-keys-to-jtsx-jsx-mode-map ()
-;;     (tide-bind-keys-to-mode-map jtsx-jsx-mode-map))
+  (defun jtsx-bind-keys-to-jtsx-jsx-mode-map ()
+    (tide-bind-keys-to-mode-map jtsx-jsx-mode-map))
 
-;;   (defun jtsx-bind-keys-to-jtsx-tsx-mode-map ()
-;;     (tide-bind-keys-to-mode-map jtsx-tsx-mode-map))
+  (defun jtsx-bind-keys-to-jtsx-tsx-mode-map ()
+    (tide-bind-keys-to-mode-map jtsx-tsx-mode-map))
 
-;;   (add-hook 'jtsx-jsx-mode-hook 'tide-bind-keys-to-jtsx-jsx-mode-map)
-;;   (add-hook 'jtsx-tsx-mode-hook 'tide-bind-keys-to-jtsx-tsx-mode-map))
+  (add-hook 'jtsx-jsx-mode-hook 'tide-bind-keys-to-jtsx-jsx-mode-map)
+  (add-hook 'jtsx-tsx-mode-hook 'tide-bind-keys-to-jtsx-tsx-mode-map))
 
 (use-package jtsx
   :ensure t
-  :after (lsp-mode)
+  :after (flycheck)
   :mode (("\\.[cm]jsx?\\'" . jtsx-jsx-mode)
          ("\\.tsx\\'" . jtsx-tsx-mode)
          ("\\.ts\\'" . jtsx-typescript-mode))
@@ -48,9 +48,10 @@
   :hook ((jtsx-jsx-mode . hs-minor-mode)
          (jtsx-tsx-mode . hs-minor-mode)
          (jtsx-typescript-mode . hs-minor-mode)
-         (jtsx-jsx-mode . lsp-deferred)
-         (jtsx-tsx-mode . lsp-deferred)
-         (jtsx-typescript-mode . lsp-deferred))
+         ;; (jtsx-jsx-mode . lsp-deferred)
+         ;; (jtsx-tsx-mode . lsp-deferred)
+         ;; (jtsx-typescript-mode . lsp-deferred)
+         )
   :custom
   ;; Optional customizations
   (js-indent-level 4)
@@ -64,11 +65,11 @@
   (jtsx-enable-all-syntax-highlighting-features t)
 
   :config
-  (defun radz/ts-lsp-plus-eslint ()
-    ;; Always run ESLint *after* the LSP checker
-    (flycheck-add-next-checker 'lsp 'javascript-eslint))
-  ;; Run this once LSP is managing the buffer
-  (add-hook 'lsp-mode-hook #'radz/ts-lsp-plus-eslint)
+  ;; (defun radz/ts-lsp-plus-eslint ()
+  ;;   ;; Always run ESLint *after* the LSP checker
+  ;;   (flycheck-add-next-checker 'lsp 'javascript-eslint))
+  ;; ;; Run this once LSP is managing the buffer
+  ;; (add-hook 'lsp-mode-hook #'radz/ts-lsp-plus-eslint)
 
   (flycheck-add-mode 'javascript-eslint 'jtsx-jsx-mode)
   (flycheck-add-mode 'javascript-eslint 'jtsx-typescript-mode)
